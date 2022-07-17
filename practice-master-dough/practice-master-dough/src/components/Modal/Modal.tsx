8fcae17f1b5bae7119a9d1af8695b2a90df9c61f
Onimusha-IO <pvpjcj@gmail.com>
2022-07-17 14:51:19 -0400

    modal background

--- practice-master-dough/practice-master-dough/src/components/Modal/Modal.tsx
+++ practice-master-dough/practice-master-dough/src/components/Modal/Modal.tsx
@@ -26,67 +26,68 @@ const Modal = () => {
   }, []);
 
   return (
-    <div className={styles.modal}>
-      <div className={styles.header}>
-        <FontAwesomeIcon icon={faCircleXmark} className={styles.iconLeft} />
-        <label>{modal.tittle}</label>
-        <FontAwesomeIcon
-          icon={faCircleXmark}
-          className={styles.iconRight}
-          onClick={() => {
-            setModal({ state: false, type: null, data: null });
-          }}
-        />
-      </div>
-      <div className={styles.formLogin}>
-        <div className={styles.field}>
-          <input id="inputId" value={id} type="text" placeholder=" " disabled />
-          <label htmlFor="inputId">Id</label>
-        </div>
-        <div className={styles.field}>
-          <input
-            id="inputName"
-            type="text"
-            value={text}
-            placeholder=" "
-            onChange={(e) => {
-              setText(e.target.value);
+    <div className={styles.modalBackGround}>
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <FontAwesomeIcon icon={faCircleXmark} className={styles.iconLeft} />
+          <label>{modal.tittle}</label>
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className={styles.iconRight}
+            onClick={() => {
+              setModal({ state: false, type: null, data: null });
             }}
           />
-          <label htmlFor="inputName">Nombre</label>
         </div>
-      </div>
-      <div className={styles.options}>
-        <div
-          className={styles.buttons}
-          onClick={() => {
-            setModal({
-              state: false,
-              type: null,
-              title: "",
-              accept: "",
-              reject: "",
-              data: null,
-            });
-          }}
-        >
-          {modal.reject}
+        <div className={styles.formLogin}>
+          <div className={styles.field}>
+            <input id="inputId" value={id} type="text" placeholder=" " disabled />
+            <label htmlFor="inputId">Id</label>
+          </div>
+          <div className={styles.field}>
+            <input
+              id="inputName"
+              type="text"
+              value={text}
+              placeholder=" "
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+            <label htmlFor="inputName">Nombre</label>
+          </div>
         </div>
-        {text !== "" ? (
-          <button
+        <div className={styles.options}>
+          <div
             className={styles.buttons}
             onClick={() => {
-              const values = modal.data ? modal.data : { id: id, name: text };
-              call(modal.type, { id: id, name: text }, cancelModal);
+              setModal({
+                state: false,
+                type: null,
+                title: "",
+                accept: "",
+                reject: "",
+                data: null,
+              });
             }}
           >
-            {modal.accept}
-          </button>
-        ) : (
-          <button className={styles.buttons} onClick={() => {}} disabled>
-            {modal.accept}
-          </button>
-        )}
+            {modal.reject}
+          </div>
+          {text !== "" ? (
+            <button
+              className={styles.buttons}
+              onClick={() => {
+                call(modal.type, { id: id, name: text }, cancelModal);
+              }}
+            >
+              {modal.accept}
+            </button>
+          ) : (
+            <button className={styles.buttons} onClick={() => {}} disabled>
+              {modal.accept}
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
